a124a4901d5db040408e806ab051c5d51abca34d
Eli Orland <eliorland@Eli-Orlands-MacBook-Pro.local>
2022-02-12 12:12:01 -0500

    pseudo working version

--- Finder.py
+++ Finder.py
@@ -31,6 +31,7 @@ class Finder:
             sizes = d[key]['sizes']
             #print(sizes)
             size_df = pd.DataFrame.from_dict(sizes,orient='index')
+            print(size_df)
             size_df['PRICE'] = d[key]['price']
             size_df = pd.concat({key: size_df}, names=['NAME'])
             df = pd.concat([df,size_df])
@@ -38,14 +39,44 @@ class Finder:
         self.Build = True
     
 
-    def find(self,waist,f_rise,b_rise,
-                     thigh,knee,hem,inseam):
-        #assert self.Build==True, "use build() before find()!"
-        req = pd.Series([None, waist, f_rise, b_rise,
+    def find(self,waist,f_rise,b_rise,thigh,knee,hem,inseam):
+        assert self.Build==True, "use build() before find()!"
+        req = pd.Series(['USER', waist, f_rise, b_rise,
                    thigh, knee, hem, inseam])
-        print(self.products.index.get_level_values(0))
+        
+        
+        #print(self.products.index.get_level_values(0))
+        candidates = {}
+        for name in self.products.index.get_level_values(0).unique():
             
-        return req
+            # work with copy of data
+            copy = self.products.loc[name].copy() 
+            
+            copy['user_meas'] = req.values # set user values
+            
+            # remove nans and price -- items not necessary
+            copy.dropna(axis=1, how='all',inplace=True) 
+            copy.drop('PRICE',axis=1,inplace=True) 
+            
+            # transpose
+            t = copy.T.set_index('TAGGED SIZE')
+            
+            # separate user and size indo
+            user = t.loc['USER']
+            inventory = t.drop('USER')
+            
+            # take differences 
+            abs_diff = inventory.sub(user.values).abs().astype(float)
+            mins = abs_diff.idxmin() # get sizes of closest values
+            cnts = mins.value_counts() # get counts of unique sizes
+            if cnts.max()>=3: # if the same size is close 3 times, add it
+                candidates[name] = {'size':cnts.idxmax(),
+                                    'Price': self.products.loc[name]['PRICE'].max()}
+            
+            
+            
+            
+        return candidates
 
         
 f = Finder()
